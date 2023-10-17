9ddbbc46315830ae762a5a5b9d37a64d3647283c
zielOS <ahsanur041@gmail.com>
2023-10-17 02:55:09 -0600

    updated

--- arch_hyprland.sh
+++ arch_hyprland.sh
@@ -4,7 +4,7 @@ cd  && git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si -r && cd
 
-yay -S hyprland-nvidia-git libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-zen-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep playerctl gradience-git adw-gtk3-git jq eww-tray-wayland-git polkit-gnome swww gtklock pamixer cliphist grimblast-git gnome-control-center thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja python-material-color-utilities zenity socat swhkd-git pyprland anyrun waybar-hyprland-git ripgrep fd python-pynvim nodejs-neovim ckb-next insync acct lsd python-pam swappy pavucontrol sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet
+yay -S hyprland-nvidia-git libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-zen-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep playerctl gradience-git adw-gtk3-git jq eww-tray-wayland-git polkit-gnome swww gtklock pamixer cliphist grimblast-git gnome-control-center thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja python-material-color-utilities zenity socat swhkd-git pyprland anyrun waybar-hyprland-git ripgrep fd python-pynvim nodejs-neovim ckb-next insync acct lsd python-pam swappy pavucontrol sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet mpv sxiv
 
 cd && mkdir ~/.npm-global && npm config set prefix '~/.npm-global'
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) 
@@ -33,6 +33,7 @@ ln -s $HOME/.dots/.config/qt5ct  $HOME/.config/
 ln -s $HOME/.dots/.config/qt6ct  $HOME/.config/
 ln -s $HOME/.dots/.config/wallpapers  $HOME/.config/
 ln -s $HOME/.dots/.config/waybar $HOME/.config/
+ln -s $HOME/.dots/.local/bin/hypr $HOME/.local/bin
 ln -s $HOME/.dots/.config/zathura  $HOME/.config/
 ln -s $HOME/.dots/.config/zsh $HOME/.config/
 ln -s $HOME/.dots/.gtkrc-2.0 $HOME/
