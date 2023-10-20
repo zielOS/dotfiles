71285487c3f07817c2d49b79d7638a0f444c26ab
zielOS <ahsanur041@gmail.com>
2023-10-20 14:28:31 -0600

    updated

--- arch_hyprland.sh
+++ arch_hyprland.sh
@@ -4,13 +4,11 @@ cd  && git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si -r && cd
 
-yay -S hyprland-nvidia-git libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-zen-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep playerctl gradience-git adw-gtk3-git jq eww-tray-wayland-git polkit-gnome swww gtklock pamixer cliphist grimblast-git gnome-control-center thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja python-material-color-utilities zenity socat swhkd-git pyprland anyrun waybar-hyprland-git ripgrep fd python-pynvim nodejs-neovim ckb-next insync acct lsd python-pam swappy pavucontrol sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet mpv sxiv
+yay -S hyprland-nvidia-git fnm zoxide libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-zen-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep playerctl gradience-git adw-gtk3-git jq eww-tray-wayland-git polkit-gnome swww gtklock pamixer cliphist grimblast-git gnome-control-center thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja python-material-color-utilities zenity socat swhkd-git pyprland anyrun waybar-hyprland-git ripgrep fd python-pynvim nodejs-neovim ckb-next insync acct lsd python-pam swappy pavucontrol sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet mpv sxiv
 
 cd && mkdir ~/.npm-global && npm config set prefix '~/.npm-global'
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) 
 
-curl -fsSL https://fnm.vercel.app/install | bash
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
 echo "Installing Dot files"
@@ -18,7 +16,8 @@ cd
 sudo rm -R ~/.config/
 cd && mkdir ~/.config  
 ln -s $HOME/.dots/.config/eww $HOME/.config/
-ln -s $HOME/.dots/.config/alacritty $HOME/.config/
+ln -s $HOME/.dots/.config/arg $HOME/.config/
+ln -s $HOME/.dots/.config/kitty $HOME/.config/
 ln -s $HOME/.dots/.config/anyrun $HOME/.config/
 ln -s $HOME/.dots/.config/btop $HOME/.config/
 ln -s $HOME/.dots/.config/ckb-next $HOME/.config/ 
