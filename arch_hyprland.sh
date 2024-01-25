ba529a9cb9fe1c9ef80e5de6824800fd7d0bd600
zielOS <ahsanur041@gmail.com>
2024-01-25 07:03:44 -0700

    updated

--- arch_hyprland.sh
+++ arch_hyprland.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-yay -S hyprland-git fnm zoxide libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland-git xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep jq polkit-gnome swww thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobjec pyprland anyrun-git ripgrep fd python-pynvim nodejs-neovim ckb-next insync acct lsd python-pam swappy pavucontrol sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet mpv kvantum refind-btrfs --noconfirm
+rustup default stable
+
+yay -S fnm hyprland-git zoxide libva-nvidia-driver-git nvidia-dkms xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep jq polkit-gnome swww thunar thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobjec pyprland anyrun-git ripgrep fd python-pynvim rkhunter chkrootkit nodejs-neovim tree-sitter-cli spotify-launcher transmission-qt stacer-bin bleachbit ckb-next insync alacritty kitty acct lsd python-pam swappy pavucontrol steam lutris devtools xournalpp mailspring logrotate ranger sassc swww ttf-jetbrains-mono-nerd ttf-font-awesome greetd-tuigreet mpv kvantum brave-bin kvantum jitterentropy-rngd hyprshade fluidsynth wine-staging fwupd aylurs-gtk-shell-git alhp-keyring alhp-mirrorlist fastfetch gnome-keyring libgnome-keyring org.freedesktop.secrets cups
+
+git clone https://github.com/donovanglover/hyprnome -b 0.2.0 && cd hyprnome && makepkg -si && cd
 
 cd && mkdir ~/.npm-global && npm config set prefix '~/.npm-global'
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) 
@@ -11,7 +15,6 @@ echo "Installing Dot files"
 cd 
 sudo rm -R ~/.config/
 cd && mkdir ~/.config  
-ln -s $HOME/.dots/.config/eww $HOME/.config/
 ln -s $HOME/.dots/.config/kitty $HOME/.config/
 ln -s $HOME/.dots/.config/alacritty $HOME/.config/
 ln -s $HOME/.dots/.config/anyrun $HOME/.config/
@@ -23,11 +26,10 @@ ln -s $HOME/.dots/.config/hypr  $HOME/.config/
 ln -s $HOME/.dots/.config/Kvantum  $HOME/.config/
 ln -s $HOME/.dots/.config/lazygit $HOME/.config/
 ln -s $HOME/.dots/.config/lsd  $HOME/.config/
-ln -s $HOME/.dots/.config/Thunar $HOME/.config/
+ln -s $HOME/.dots/Thunar $HOME/
 ln -s $HOME/.dots/.config/qt5ct  $HOME/.config/
 ln -s $HOME/.dots/.config/qt6ct  $HOME/.config/
 ln -s $HOME/.dots/.config/wallpapers  $HOME/.config/
-ln -s $HOME/.dots/.config/waybar $HOME/.config/
 ln -s $HOME/.dots/.local/bin/hypr $HOME/.local/bin
 ln -s $HOME/.dots/.config/zathura  $HOME/.config/
 ln -s $HOME/.dots/.config/zsh $HOME/.config/
@@ -45,3 +47,13 @@ ln -s $HOME/.dots/gentoo_setup $HOME/
 echo "Setup Systemd"
 sudo systemctl set-default graphical.target 
 systemctl --user enable --now wireplumber.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service
+
+echo "Setup Git"
+cd 
+git config --global user.name "zielOS"
+git config --global user.email "ahsanur041@gmail.com"
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
+
+cd && sudo cp -R ~/.dots/variables.sh /etc/profile.d/
+
+sudo systemctl enable --now jitterentropy rngd haveged cups ckb-next-daemon avahi-daemon
