2373987bdf6e9d58d47f951e276e667fe71857c9
Ahsanur Rahman <ahsan@recovery.lan>
2024-08-07 16:25:56 +0600

    updated

---
+++ debian-scripts/makekali.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+apt update
+apt -y install wget gnupg dirmngr
+wget -q -O - https://archive.kali.org/archive-key.asc | gpg --import
+gpg --keyserver hkp://keys.gnupg.net --recv-key 44C6513A8E4FB3D30875F758ED444FF07D8D0BF6
+echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
+apt update
+apt -y upgrade
+apt -y dist-upgrade
+apt -y autoremove --purge
