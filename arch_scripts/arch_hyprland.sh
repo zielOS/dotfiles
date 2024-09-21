47f708f934a6f70e7770260d2ea27c1ed982ec4e
zielOS <ahsanur041@gmail.com>
2024-09-21 03:10:45 -0400

    updated

--- arch_scripts/arch_hyprland.sh
+++ arch_scripts/arch_hyprland.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-sudo pacman -S git
-git clone https://aur.archlinux.org/yay-bin && cd yay-bin
-makepkg -si && cd
-
-yay -S zoxide nvidia-open-dkms nvidia-utils xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva libva-nvidia-driver-git pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep fd jq swww mate-polkit thunar thunar-volman thunar-media-tags-plugin thunar-archive-plugin tumbler file-roller xdg-user-dirs xdg-user-dirs-gtk dbus-python pyprland rkhunter chkrootkit tree-sitter-cli spotify-launcher bleachbit insync alacritty kitty acct lsd python-pam swappy steam lutris devtools xournalpp mailspring logrotate ranger sassc ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-ubuntu-font-family mpv kvantum brave-bin jitterentropy-rngd wine-staging fwupd aylurs-gtk-shell-git fastfetch gnome-keyring libgnome-keyring org.freedesktop.secrets cups arch-audit typescript meson gjs gtk3 gtk4 gnome-bluetooth-3.0 upower networkmanager gobject-introspection libdbusmenu-gtk3 libsoup3 unzip zip unrar btrfs-progs chrony dosfstools zathura zathura-pdf-poppler papirus-icon-theme wget curl python-pip zsh python-pynvim mesa rng-tools haveged lazygit lynis audit sysstat acpid fzf flatpak fdupes wl-clipboard vulkan-tools vulkan-headers vulkan-validation-layers nodejs nodejs-neovim npm transmission-gtk cmake boost tldr emacs-wayland snapper snap-pac snap-pac-grub hyprlang hyprcursor hyprwayland-scanner hypridle hyprlock hyprnome hyprdim hyprpaper hyprpicker hyprland hyprshade wf-recorder rust fnm brightnessctl cliphist xdg-desktop-portal-hyprland gtk-layer-shell gtk4-layer-shell starship zoxide
-
 echo "Installing Dot files"
 cd
 sudo rm -R $HOME/.local/
