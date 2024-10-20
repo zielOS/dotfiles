7e34ebc79fb343d2f7ef5100c4787fc9e09f4b70
zielOS <ahsanur041@gmail.com>
2024-10-20 13:31:32 +0600

    updated

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland/";
+    hyprland-contrib.url = "github:hyprwm/contrib";
+    hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
+    matugen.url = "github:InioX/matugen";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,21 +17,32 @@
       url = "github:Aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = { nixpkgs, home-manager, ags, ... }:
+  outputs = { nixpkgs, home-manager, ags, self, ... }:
     let
+      inputs = self.inputs;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      hmModule = inputs.home-manager.nixosModules.home-manager;
+      home-manager = {
+        useUserPackages = true;
+        useGlobalPkgs = true;
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit self;
+        };
+      users.ahsan = import ./home;
+      };
+
     in {
       homeConfigurations."ahsan" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ 
-          ./home.nix 
-
+        modules = [  
+          hmModule
+          {inherit home-manager;}
           {
             home.packages = [
               ags.packages.${system}.default
@@ -34,7 +50,7 @@
           
           }
         ];
-
+        specialArgs = {inherit inputs;};
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
